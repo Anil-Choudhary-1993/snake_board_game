b6888838f819ac0a42395b219bd5ddd43b580a6f
Anil-Choudhary-1993 <anchoudhary@sandvine.com>
2021-06-22 20:30:09 +0530

    Updates

---
+++ precache-manifest.440a4153c96c7068e1bcc5305cb22934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfad6b5a18cb0c0e61365dee3da7854",
+    "url": "/snake_board_game/index.html"
+  },
+  {
+    "revision": "ec84a20706b95f957a83",
+    "url": "/snake_board_game/static/css/main.5700533c.chunk.css"
+  },
+  {
+    "revision": "777898e2cfcc74605f35",
+    "url": "/snake_board_game/static/js/2.eb7d7366.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/snake_board_game/static/js/2.eb7d7366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec84a20706b95f957a83",
+    "url": "/snake_board_game/static/js/main.2cc8ccc6.chunk.js"
+  },
+  {
+    "revision": "026c1225f0b9e5dbb973",
+    "url": "/snake_board_game/static/js/runtime-main.f45994bb.js"
+  }
+]);
